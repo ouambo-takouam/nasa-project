190ad231f42170a577b6d4f9651bb539b238128e
patrick ouambo <patrickouambo@gmail.com>
2023-03-06 11:29:57 +0100

    The Planets Model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,36 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+fs.createReadStream(__dirname + '../data/kepler_data.csv') // stream => eventEmitter
+	.pipe(
+		// parsing data
+		parse({
+			comment: '#',
+			columns: true,
+		})
+	)
+	.on('data', (data) => {
+		if (isHabitablePlanet(data)) {
+			habitablePlanets.push(data);
+		}
+	})
+	.on('error', (error) => console.log(error))
+	.on('end', () => {
+		console.log(`${habitablePlanets.length} habitable planets found!`);
+		console.log('done');
+	});
+
+function isHabitablePlanet(planet) {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	);
+}
+
+module.exports = {
+	planets: habitablePlanets,
+};
