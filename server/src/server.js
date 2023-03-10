5dc677354213474d9865e5a2c67468e8b9981d28
patrick ouambo <patrickouambo@gmail.com>
2023-03-10 09:09:46 +0100

    connecting to mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
 const http = require('http');
 const app = require('./app'); // listener
+const mongoose = require('mongoose');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+	'mongodb+srv://nasa-api:c9TYlfMXehEHGOnH@nasacluster.tjzx2xz.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+	console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (error) => {
+	console.error(error);
+});
+
 async function startServer() {
+	await mongoose.connect(MONGO_URL);
 	await loadPlanetsData();
 
 	server.listen(PORT, () => {
